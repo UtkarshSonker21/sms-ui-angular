c251073283e3a795fc4ba43836ec5526dfb6fed0
UtkarshSonker21 <utkarshsonker.21@email.com>
2026-08-24 20:26:40 +0530

    added backend url for api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
 
   // apiUrl: 'https://localhost:7000/api/',
-  apiUrl: 'https://sm-api.runasp.net/api/',
+  // apiUrl: 'https://sm-api.runasp.net/api/',
+  apiUrl: 'https://kafatapi.ifnoss.us/api/',
 
   appName: 'Scholarship Management System',
 
